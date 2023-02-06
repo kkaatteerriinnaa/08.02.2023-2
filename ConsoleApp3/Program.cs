8617f3b3aa5fc12c6c2e86eea003472f37fe6a4c
Катерина Буравлева <kkaatteerriinnaa2003@gmail.com>
2023-02-06 22:38:04 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int even=0, odd=0;
+            //int[] arr = new int[10];
+            //Random a = new Random();
+            //for(int i =0;i<arr.Length;i++)
+            //{
+            //    arr[i] = a.Next(1,100);
+            //    Console.WriteLine(arr[i]);
+            //    if(arr[i]/2==0)
+            //    {
+            //        even++;
+            //    }
+            //    else
+            //    {
+            //        odd++;
+            //    }
+            //}
+            //Console.WriteLine("Even:" + even);
+            //Console.WriteLine("Odd:" + odd);
+
+            //int[] arr = new int[15];
+            //Random a = new Random();
+            //Console.WriteLine("Введите число:");
+            //int num = int.Parse(Console.ReadLine());
+            //for(int i=0;i<arr.Length;i++)
+            //{
+            //    arr[i] = a.Next(1, 100);
+            //    if(arr[i]<num)
+            //    {
+            //        Console.WriteLine(arr[i] + " ");
+            //    }
+            //}
+
+            //int[] arr = new int[15];
+            //Random a = new Random();
+            //int count = 0;
+            //Console.WriteLine("Введите первое число:");
+            //int n1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите второе число:");
+            //int n2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите третье число:");
+            //int n3 = int.Parse(Console.ReadLine());
+            //for(int i=0;i<arr.Length;i++)
+            //{
+            //    arr[i] = a.Next(1, 10);
+            //    Console.WriteLine(arr[i]);
+            //    if(arr[i]==n1 && arr[i+1]==n2 && arr[i+2]==n3)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine("Совпдений: " + count);
+        }
+    }
+}
